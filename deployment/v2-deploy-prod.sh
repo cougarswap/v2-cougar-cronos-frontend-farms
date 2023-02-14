6e47df79cdfbebba0328228ab7a95d79d30e8199
cougarswapio <cougarswapio@gmail.com>
2023-02-14 10:13:25 +0700

    update deployment cloudfront id

--- deployment/v2-deploy-prod.sh
+++ deployment/v2-deploy-prod.sh
@@ -1,3 +1,3 @@
 aws s3 sync './build' s3://v2-cougar-web-farm-cronos/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --delete --exclude '.git/*' --exclude '.vscode/*' --exclude 'package.json'
 
-aws cloudfront create-invalidation --distribution-id E2B9XWCCUK6LWQ --paths "/*"
+aws cloudfront create-invalidation --distribution-id E15GVMLDDJD0VK --paths "/*"
